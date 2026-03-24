e08d648e5ed324815694592b648abf6516913ed4
John Weidner <john.weidner@verygood.ventures>
2026-03-24 17:51:15 -0500

    Build for GitHub Pages deploy

---
+++ deploy/static-example/main.client.dart.js_17.part.js
@@ -0,0 +1,95 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,G,F,C,A={qi:function qi(d,e,f,g,h,i){var _=this
+_.c=d
+_.d=e
+_.e=f
+_.f=g
+_.r=h
+_.a=i},jG:function jG(d,e,f,g,h,i){var _=this
+_.d=d
+_.f=e
+_.r=f
+_.w=g
+_.y=h
+_.a=i},AJ:function AJ(){},vB:function vB(){var _=this
+_.e=_.d=!0
+_.c=_.a=null},FJ:function FJ(d){this.a=d},FH:function FH(d){this.a=d},FK:function FK(d){this.a=d},FG:function FG(d){this.a=d},
+RJ(){return new A.hB(null)},
+hB:function hB(d){this.a=d},
+zj:function zj(){}},D,E
+B=c[0]
+G=c[7]
+F=c[6]
+C=c[9]
+A=a.updateHolder(c[4],A)
+D=c[17]
+E=c[13]
+A.qi.prototype={
+a0(d){var x,w,v,u,t=this,s=null,r="px",q=B.e([],y.j),p=t.f
+if(B.U(p.minWidth)!==1/0){x=B.U(p.minWidth)
+x=x==null?s:new C.e6(r,x)}else x=s
+if(B.U(p.maxWidth)!==1/0){w=B.U(p.maxWidth)
+w=w==null?s:new C.e6(r,w)}else w=s
+if(B.U(p.minHeight)!==1/0){v=B.U(p.minHeight)
+v=v==null?s:new C.e6(r,v)}else v=s
+if(B.U(p.maxHeight)!==1/0){u=B.U(p.maxHeight)
+u=u==null?s:new C.e6(r,u)}else u=s
+q.push(C.Kz(s,u,w,v,x))
+x=B.e([],y.p)
+x.push(t.r)
+return E.dA(x,t.d,t.c,s,new C.nO(q))}}
+A.jG.prototype={
+bv(){return new A.vB()}}
+A.vB.prototype={
+aX(){var x=this
+x.ip()
+$.PT().fo(new A.FJ(x))
+x.a.w.fo(new A.FK(x))},
+a0(d){var x,w,v=this,u=null
+if(v.d||v.e){x=v.a
+return new A.qi(u,x.d,u,x.f,x.r,u)}x=v.a
+x.toString
+B.p7("flutter")
+w=x.y.$0()
+return F.St(x.d,x.f,u,x.r,u,y.g.a(w))}}
+A.hB.prototype={
+a0(d){var x=B.yn("flutter_app","")
+return new A.jG("flutter-container",{minWidth:300,maxWidth:1/0,minHeight:400,maxHeight:1/0},D.xi,x,new A.zj(),null)}}
+var z=a.updateTypes(["jM()"])
+A.AJ.prototype={
+$1(d){B.p7("flutter")
+return F.Su()},
+$S:22}
+A.FJ.prototype={
+$0(){var x=this.a
+if(x.c==null)return
+x.bd(new A.FH(x))},
+$S:7}
+A.FH.prototype={
+$0(){this.a.d=!1},
+$S:0}
+A.FK.prototype={
+$0(){var x=this.a
+if(x.c==null)return
+x.bd(new A.FG(x))},
+$S:7}
+A.FG.prototype={
+$0(){this.a.e=!1},
+$S:0}
+A.zj.prototype={
+$0(){B.p7("flutter_app")
+return G.SO()},
+$S:z+0};(function inheritance(){var x=a.inheritMany,w=a.inherit
+x(E.bu,[A.qi,A.hB])
+w(A.jG,E.dr)
+w(A.AJ,B.cv)
+w(A.vB,E.bV)
+x(B.eb,[A.FJ,A.FH,A.FK,A.FG,A.zj])})()
+B.eD(b.typeUniverse,JSON.parse('{"qi":{"bu":[],"ap":[]},"jG":{"dr":[],"ap":[]},"vB":{"bV":["jG"],"bV.T":"jG"},"hB":{"bu":[],"ap":[]}}'))
+var y={p:B.a_("p<ap>"),j:B.a_("p<dY>"),g:B.a_("a8?")};(function constants(){var x=a.makeConstList
+D.w8=new B.aJ("Loading Flutter app...",null)
+D.q8=x([D.w8],y.p)
+D.xi=new E.j4(null,null,null,D.q8,null)})();(function lazyInitializers(){var x=a.lazyFinal
+x($,"YH","PT",()=>B.yn("flutter","").aL(new A.AJ(),B.a_("~")))})()};
+(a=>{a["NE7L/Ybqq+1aoJkeHxDOs0Bo7WI="]=a.current})($__dart_deferred_initializers__);
+//# sourceMappingURL=main.client.dart.js_17.part.js.map
